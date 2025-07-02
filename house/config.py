6e04e60fbbe4b1de227df7ff776670cc858e8e20
wuzhiheng0011 <wzh_0615@foxmail.com>
2025-07-02 18:55:10 +0800

    Update config.py

--- house/config.py
+++ house/config.py
@@ -6,6 +6,16 @@ pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 
+app.config['DB_CONFIG'] = {
+    'host':        'localhost',
+    'port':        3306,
+    'user':        'root',
+    'password':    '123456',
+    'db':          'house',
+    'charset':     'utf8mb4',
+    'cursorclass': None
+}
+
 # url的格式为：数据库的协议：//用户名：密码@ip地址：端口号（默认可以不写）/数据库名
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:123456@localhost:3306/house"
 # 动态追踪数据库的修改. 性能不好. 且未来版本中会移除. 目前只是为了解决控制台的提示才写的
