82f6a56cf7b321acd0684376b98c011ab6687e67
enmanka <118905493+enmanka@users.noreply.github.com>
2025-07-10 14:19:01 +0800

    修改7.10

--- house/config.py
+++ house/config.py
@@ -1,26 +1,24 @@
-# config.py
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 import pymysql
 pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 
 app.config['DB_CONFIG'] = {
-    'host':        '192.168.49.134',
+    'host':        'localhost',
     'port':        3306,
     'user':        'root',
-    'password':    'root',
-    'db':          'iuxn',
+    'password':    '123456',
+    'db':          'shixun',
     'charset':     'utf8mb4',
     'cursorclass': None
 }
 
-# 直接连接到虚拟机上的 MySQL
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    "mysql+pymysql://root:root@192.168.49.134:3306/ixun?charset=utf8mb4"
-)
+# url的格式为：数据库的协议：//用户名：密码@ip地址：端口号（默认可以不写）/数据库名
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:123456@localhost:3306/shixun"
+# 动态追踪数据库的修改. 性能不好. 且未来版本中会移除. 目前只是为了解决控制台的提示才写的
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
+# 创建数据库的操作对象
 db = SQLAlchemy(app)
